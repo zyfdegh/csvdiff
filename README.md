e4a05105266961645c2c2209d218b7178b205fe2
zhangyifa <zhangyifa@lanjingren.com>
2020-04-01 22:57:33 +0800

    update docs

--- README.md
+++ README.md
@@ -6,4 +6,6 @@ diff text files and print the adds
 $ ./diffcsv testdata/a.csv testdata/b.csv
 4
 5
+
+$ time ./csvdiff testdata/a-full.csv testdata/b-full.csv > testdata/c-full-multi.csv
 ```
--- main.go
+++ main.go
@@ -17,11 +17,14 @@ func main() {
 	diffseq(f1, f2)
 }
 
+// 1. 不加 break，600 亿次比较
 // 258.06s user 1.69s system 99% cpu 4:21.14 total
-// 不加 break，600 亿次比较
 //
+// 2. 加 break，减少 29.1% 时间
 // 183.91s user 2.06s system 93% cpu 3:19.30 total
-// 加 break，减少 29.1% 时间
+//
+// 3. 多核，相比【2】减少 5.5% 时间
+// 173.17s user 1.60s system 280% cpu 1:02.29 total
 func diffseq(f1, f2 string) {
 	arr1, err := readLines(f1)
 	if err != nil {
