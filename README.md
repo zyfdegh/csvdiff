8914f514e6a49dfd5083fdeff116b166454b7606
zyfdegh <zyfdegg@gmail.com>
2020-04-02 22:39:35 +0800

    update docs

--- README.md
+++ README.md
@@ -9,3 +9,20 @@ $ ./diffcsv testdata/a.csv testdata/b.csv
 
 $ time ./csvdiff testdata/a-full.csv testdata/b-full.csv > testdata/c-full-multi.csv
 ```
+
+```sh
+1. diff(), Intel i5-7500
+183.91s user 2.06s system 93% cpu 3:19.30 total
+
+2. diffmulti(), Intel i5-7500, 68.8% less time compared to [1]
+173.17s user 1.60s system 280% cpu 1:02.29 total
+
+3. diffmulti()，Intel i5-2520M
+537.72s user 1.12s system 393% cpu 2:16.95 total
+
+4. diffmulti()，Intel i5-7267U
+348.91s user 1.44s system 362% cpu 1:36.56 total
+
+5. diffmulti(), Intel Atom N270
+about 15min
+```
--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"runtime"
 )
 
 func main() {
@@ -14,17 +15,10 @@ func main() {
 	f1, f2 := os.Args[1], os.Args[2]
 	log.Printf("reading: %s, %s\n", f1, f2)
 
+	runtime.GOMAXPROCS(runtime.NumCPU())
 	diffseq(f1, f2)
 }
 
-// 1. 不加 break，600 亿次比较
-// 258.06s user 1.69s system 99% cpu 4:21.14 total
-//
-// 2. 加 break，减少 29.1% 时间
-// 183.91s user 2.06s system 93% cpu 3:19.30 total
-//
-// 3. 多核，相比【2】减少 5.5% 时间
-// 173.17s user 1.60s system 280% cpu 1:02.29 total
 func diffseq(f1, f2 string) {
 	arr1, err := readLines(f1)
 	if err != nil {
